8522616324a3027c10e909da46c94daaee83496f
DonFra <43624898+lindonato@users.noreply.github.com>
2023-07-25 12:55:30 +0200

    Create multiact.cmd

---
+++ multiact.cmd
@@ -0,0 +1,8 @@
+  # https://github.com/abbodi1406
+# wget https://github.com/abbodi1406/KMS_VL_ALL_AIO/raw/master/KMS_VL_ALL_AIO.cmd
+gh repo clone abbodi1406/KMS_VL_ALL_AIO
+
+  # https://github.com/massgravel
+# wget https://github.com/massgravel/Microsoft-Activation-Scripts/blob/master/MAS/All-In-One-Version/MAS_AIO.cmd
+gh repo clone massgravel/Microsoft-Activation-Scripts
+  
