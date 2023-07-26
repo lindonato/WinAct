608448ef6934672e677e30e81a3d3a1093d437a0
DonFra <43624898+lindonato@users.noreply.github.com>
2023-07-26 08:35:41 +0200

    Update multiact.cmd

--- multiact.cmd
+++ multiact.cmd
@@ -3,6 +3,7 @@
 gh repo clone abbodi1406/KMS_VL_ALL_AIO
 
   # https://github.com/massgravel
+# irm https://massgrave.dev/get | iex
 # wget https://github.com/massgravel/Microsoft-Activation-Scripts/blob/master/MAS/All-In-One-Version/MAS_AIO.cmd
 gh repo clone massgravel/Microsoft-Activation-Scripts
   
